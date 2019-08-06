52090762dd840ac6989340d0ddc22f010029acc4
ReginaLiang <53214295+ReginaLiang@users.noreply.github.com>
2019-08-06 16:37:27 +0800

    Add files via upload

---
+++ precache-manifest.8cedf80d409699bc1c636591098b08e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5dae66f82e67bdbded11",
+    "url": "/my-inferno-app/static/js/runtime~main.5dae66f8.js"
+  },
+  {
+    "revision": "dcbcc61e759ef2fbef14",
+    "url": "/my-inferno-app/static/js/main.dcbcc61e.chunk.js"
+  },
+  {
+    "revision": "7627e4c6660cc980d02f",
+    "url": "/my-inferno-app/static/js/2.7627e4c6.chunk.js"
+  },
+  {
+    "revision": "dcbcc61e759ef2fbef14",
+    "url": "/my-inferno-app/static/css/main.5fb2ed42.chunk.css"
+  },
+  {
+    "revision": "5fe23bf14c4826ac7a0fd94859d7216f",
+    "url": "/my-inferno-app/index.html"
+  }
+];
